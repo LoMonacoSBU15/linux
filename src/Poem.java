5e58e8e6cd74b3a9ae7f09ccc31bda481ac0c882
Wheatley <wheatley@ubuntu.ubuntu-domain>
2017-04-01 21:06:34 -0400

    first commit

---
+++ src/Poem.java
@@ -0,0 +1,12 @@
+
+public class Poem {
+
+	public static void main(String[] args) throws InterruptedException {
+		for(int i = 0;i<10;i++) {
+			Thread.sleep(1000);
+		System.out.println("Paige is sexy");
+		}
+
+	}
+
+}
